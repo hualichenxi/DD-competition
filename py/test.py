b66a1405dc825ceb43ae88b654063bf94a264921
data <data@node3.(none)>
2016-05-25 20:37:27 +0800

    train.py

--- py/test.py
+++ py/test.py
@@ -3,33 +3,6 @@ import numpy
 import datetime
 from sklearn.externals import joblib
 
-
-fs = ['gap1','gap2','gap3','ifmpeak','ifepeak','iffestival','ifweekday']
-
-print 'load data'
-
-te_x = numpy.array([[]])
-for f in fs:
-	te = loadColumns('../data/test_x_'+f+'/000000_0')
-	te_x = numpy.concatenate((te_x,te),axis=1)
-
-print 'testing ...\n'
-
-starttime = datetime.datetime.now()
-regr = joblib.load("model/model.m")
-te_y = regr.predict(te_d)
-
-endtime1 = datetime.datetime.now()
-print str((endtime1 - starttime).seconds) + ' seconds used.\n'
-
-te_t = loadTestT('../data/test_t/000000_0')
-f = open('results.csv','w')
-for i,y in enumerate(te_y):
-	o = te_t[i]+','+str(y) + '\n'
-	f.write(o)
-f.close()
-
-
 def loadColumns(s):
 	f = open(s)
 	lines = f.readlines()
@@ -38,7 +11,7 @@ def loadColumns(s):
 	for line in lines:
 		line = line.replace('\\N','0')
 		a.append([float(i) for i in line.strip().split(' \x01')])
-	return numpy.array(ar)
+	return numpy.array(a)
 
 def loadTestT(s):
 	f = open(s)
@@ -46,3 +19,35 @@ def loadTestT(s):
 	f.close()
 	a = [l.replace('\x01','').strip() for l in lines]
 	return a
+
+
+fs = ['gap1','gap2','gap3','ifmpeak','ifepeak','iffestival','ifweekday','dayofweek','dayofweekday']
+
+print 'load data'
+te_t = loadTestT('../data/test_t/000000_0')
+
+te_x = numpy.empty([len(te_t),0])
+
+for f in fs:
+	te = loadColumns('../data/test_x_'+f+'/000000_0')
+	te_x = numpy.concatenate((te_x,te),axis=1)
+print "feature shape ", te_x.shape
+print "target shape", len(te_t)
+print 'testing ...\n'
+
+starttime = datetime.datetime.now()
+regr = joblib.load("model/model.m")
+te_y = regr.predict(te_x)
+
+endtime1 = datetime.datetime.now()
+print str((endtime1 - starttime).seconds) + ' seconds used.\n'
+
+
+f = open('results.csv','w')
+for i,y in enumerate(te_y):
+	o = te_t[i]+','+str(y) + '\n'
+	f.write(o)
+f.close()
+
+
+
--- py/train.py
+++ py/train.py
@@ -3,23 +3,10 @@ import numpy
 import datetime
 from sklearn.externals import joblib
 from sklearn import linear_model
-
-fs = ['gap1','gap2','gap3','ifmpeak','ifepeak','iffestival','ifweekday']
-
-print 'load data...\n'
-tr_y = loadColumns('../data/train_y/000000_0')
-tr_x = numpy.array([[]])
-for f in fs:
-	tr = loadColumns('../data/train_x_'+f+'/000000_0')
-	tr_x = numpy.concatenate((tr_x,tr),axis=1)
-
-starttime = datetime.datetime.now()
-regr = linear_model.LinearRegression()
-regr.fit(tr_x,tr_y[:,2])
-joblib.dump(regr, "model/model.m")
-
-endtime1 = datetime.datetime.now()
-print str((endtime1 - starttime).seconds) + ' seconds used.\n'
+from sklearn import linear_model
+from sklearn import cross_validation
+from sklearn import svm
+from sklearn.ensemble import RandomForestRegressor
 
 def loadColumns(s):
 	f = open(s)
@@ -30,3 +17,71 @@ def loadColumns(s):
 		line = line.replace('\\N','0')
 		a.append([float(i) for i in line.strip().split(' \x01')])
 	return numpy.array(a)
+
+fs = ['gap1','gap2','gap3','ifmpeak','ifepeak','iffestival','ifweekday','dayofweek','dayofweekday']
+
+print 'load data...\n'
+tr_y = loadColumns('../data/train_y/000000_0')
+tr_x = numpy.empty([tr_y.shape[0],0])
+for f in fs:
+	tr = loadColumns('../data/train_x_'+f+'/000000_0')
+	print f
+#	print tr.shape
+	tr_x = numpy.concatenate((tr_x,tr),axis=1)
+
+cv = cross_validation.ShuffleSplit(tr_y.shape[0], n_iter=3, test_size=0.2,random_state=0)
+
+print "linear mode Ridge"
+starttime = datetime.datetime.now()    
+tr_score,te_score=0.0,0.0
+for train, test in cv:    
+	svc = linear_model.Ridge().fit(tr_x[train], tr_y[train,2])
+	tr_score = tr_score + svc.score(tr_x[train], tr_y[train,2])
+	te_score = te_score + svc.score(tr_x[test], tr_y[test,2])
+print("train score: {0:.3f}, test score: {1:.3f}\n".format(tr_score/cv.n_iter, te_score/cv.n_iter))
+endtime = datetime.datetime.now()
+print str((endtime - starttime).seconds) + ' seconds used.\n'
+    
+print "linear model LinearRegression"    
+starttime = datetime.datetime.now()    
+tr_score,te_score=0.0,0.0
+for train, test in cv:    
+	svc = linear_model.LinearRegression().fit(tr_x[train], tr_y[train,2])
+	tr_score = tr_score + svc.score(tr_x[train], tr_y[train,2])
+	te_score = te_score + svc.score(tr_x[test], tr_y[test,2])
+print("train score: {0:.3f}, test score: {1:.3f}\n".format(tr_score/cv.n_iter, te_score/cv.n_iter))
+endtime = datetime.datetime.now()
+print str((endtime - starttime).seconds) + ' seconds used.\n'
+ 
+#print "SVR(kernel='linear',C=10,gamma=.01)"
+#starttime = datetime.datetime.now()    
+#tr_score,te_score=0.0,0.0
+#for train, test in cv:
+#	svc = svm.SVR(kernel ='linear', C = 10, gamma = .01).fit(tr_x[train], tr_y[train,2])
+#	tr_score = tr_score + svc.score(tr_x[train], tr_y[train,2])
+#	te_score = te_score + svc.score(tr_x[test], tr_y[test,2])
+#print("train score: {0:.3f}, test score: {1:.3f}\n".format(tr_score/cv.n_iter, te_score/cv.n_iter))
+#endtime = datetime.datetime.now()
+#print str((endtime - starttime).seconds) + ' seconds used.\n'
+ 
+print "Random Forest"    
+starttime = datetime.datetime.now()    
+tr_score,te_score=0.0,0.0
+for train, test in cv:    
+	svc = RandomForestRegressor(n_estimators = 3).fit(tr_x[train], tr_y[train,2])
+	tr_score = tr_score + svc.score(tr_x[train], tr_y[train,2])
+	te_score = te_score + svc.score(tr_x[test], tr_y[test,2])
+print("train score: {0:.3f}, test score: {1:.3f}\n".format(tr_score/cv.n_iter, te_score/cv.n_iter))
+endtime = datetime.datetime.now()
+print str((endtime - starttime).seconds) + ' seconds used.\n'
+ 
+
+print 'train model'
+starttime = datetime.datetime.now()
+regr = linear_model.LinearRegression()
+regr.fit(tr_x,tr_y[:,2])
+joblib.dump(regr, "model/model.m")
+endtime = datetime.datetime.now()
+print str((endtime - starttime).seconds) + ' seconds used.\n'
+
+
